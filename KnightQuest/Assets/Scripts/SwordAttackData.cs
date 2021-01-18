c03104a3bddfc4d06f26ac0417aee83e372969ce
Timofey Peshin <timoffex@gmail.com>
2021-01-17 21:25:48 -0800

    SwordAttackData is persistable

--- KnightQuest/Assets/Scripts/SwordAttackData.cs
+++ KnightQuest/Assets/Scripts/SwordAttackData.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-sealed class SwordAttackData : MonoBehaviour
+sealed class SwordAttackData : PersistableComponent
 {
     public float attackStrength = 1;
 
@@ -11,4 +9,20 @@ sealed class SwordAttackData : MonoBehaviour
     [Tooltip("Extra attack range to add on top of the character's weapon radius for AIs to"
              + " determine when to swing the sword.")]
     public float extraAttackRange;
+
+    public override void Save(GameDataWriter writer)
+    {
+        base.Save(writer);
+        writer.WriteFloat(attackStrength);
+        writer.WriteFloat(attackCooldown);
+        writer.WriteFloat(extraAttackRange);
+    }
+
+    public override void Load(GameDataReader reader)
+    {
+        base.Load(reader);
+        attackStrength = reader.ReadFloat();
+        attackCooldown = reader.ReadFloat();
+        extraAttackRange = reader.ReadFloat();
+    }
 }
